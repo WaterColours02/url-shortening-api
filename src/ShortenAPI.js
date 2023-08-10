5845471b8c9c080be3a031419b3dd3f3b26730a1
Hunter Cheveldave <HunterCheveldave@gmail.com>
2023-08-09 18:54:24 -0700

    started SHORTEN API app work to complete desktop v

---
+++ src/ShortenAPI.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+
+
+
+export default function ShortenAPI() {
+ 
+    const [shortenedLink, setShortenedLink] = useState('');
+    const [linkToShorten, setLinkToShorten] = useState('');
+
+
+    handleChange = (e) => {}
+
+
+
+    const apiKey = 'YOUR_API_KEY';
+    const urlToShorten = 'https://www.example.com/your-long-url';
+    const apiEndpoint = 'https://api.example.com/shorten'; // Replace with actual API endpoint
+    
+    const headers = {
+      'Authorization': `Bearer ${apiKey}`,
+      'Content-Type': 'application/json',
+    };
+    
+    const requestData = {
+      url: urlToShorten,
+    };
+    
+    fetch(apiEndpoint, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(requestData),
+    })
+      .then(response => response.json())
+      .then(data => {
+        const shortenedUrl = data.short_link;
+        console.log('Shortened URL:', shortenedUrl);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+    
+
+
+
+    return(
+ 
+       
+
+
+
+
+
+    <div className='shorten-link-form-container'>
+      <form className='shorten-link-form'> 
+        <input className='shorten-link-input' placeholder='Shorten a link here...'></input>
+        <button className='shorten-link-form-button'>Shorten it !</button>
+        <p className='shorten-link-alert-text'></p>
+      </form>
+    </div>
+
+    )
+
+} 
+
